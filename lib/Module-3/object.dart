44b6e01cdf92a138747f9feae3b6c56333be7bc6
estiakahamed01 <tasin220116@diit.edu.bd>
2025-11-07 17:51:29 +0600

    Added Try Catch & Object

---
+++ lib/Module-3/object.dart
@@ -0,0 +1,27 @@
+import 'mobile.dart';
+
+main() {
+  Student student1 = Student(); // obj created
+  student1.studentName = 'Rahim';
+  student1.age = 25;
+  student1.address = 'Dhaka';
+
+ student1.StudentExam();
+
+
+
+}
+
+
+
+class Student {
+  late String studentName;
+  late int age;
+  late String address;
+   String ? gName;
+
+
+   StudentExam(){ //method
+     print('Exam on 5th Nov');
+   }
+}
